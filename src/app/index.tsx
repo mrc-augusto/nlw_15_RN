2cfa36e24b57f59983444a8e611a1fabe6e672cd
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2024-06-10 17:34:57 -0300

    first day

---
+++ src/app/index.tsx
@@ -0,0 +1,39 @@
+import { CategoryButton } from '@/components/category-button'
+import { Header } from '@/components/header'
+import {View, FlatList} from 'react-native'
+
+import {CATEGORIES} from '@/utils/data/products'
+import { useState } from 'react'
+
+export default function Home(){
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelected(selectedCategory: string){
+    setCategory(selectedCategory)
+  }
+
+  return(
+    <View className="flex-1 pt-8">
+      <Header title='Faça seu pedido' cartQuantityItems={1}/>
+      
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({item}) => (
+          <CategoryButton 
+            title={item} 
+            isSelected={item === category} 
+            onPress={() => 
+            handleCategorySelected(item)}
+            />
+          )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className='max-h-10 mt-5'
+        contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+      />
+
+    </View>
+  )
+}
+
